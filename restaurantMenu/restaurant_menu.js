c696a92b9d073c0f32962c8054d9a6dbba253795
Sreeharivs1983 <sreeharivs69@gmail.com>
2026-02-15 01:02:00 -0500

    Added Restaurant Menu Iteration Lab

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,29 @@
+// Arrays
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+// Using map() for Breakfast Menu
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => 
+    `<p>Item ${index + 1}: ${item}</p>`
+).join('');
+
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+// Using forEach() for Main Course Menu
+let mainCourseItem = '';
+
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+// Using for loop for Dessert Menu
+let dessertItem = '';
+
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
